08331d2ccdaf57769cf812db54b28ec846fd0304
Victor Matheus <victormatheus507@gmail.com>
2023-09-05 15:00:03 -0300

    reset files

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,2 @@
-import pluginSlide from "./pluginSlide.js";
-import { URL_BASE } from "./global.js";
-pluginSlide("div");
-URL_BASE;
-const livro = {
-    nome: 'salve',
-    preco: 20
-};
+"use strict";
 //# sourceMappingURL=script.js.map
