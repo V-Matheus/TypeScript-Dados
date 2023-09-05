57a45d4b329bbe3d91e2a6d0ee9d2c5ebe8223ae
Victor Matheus <victormatheus507@gmail.com>
2023-09-05 14:52:21 -0300

    first commit

---
+++ dist/script.js
@@ -0,0 +1,9 @@
+import pluginSlide from "./pluginSlide.js";
+import { URL_BASE } from "./global.js";
+pluginSlide("div");
+URL_BASE;
+const livro = {
+    nome: 'salve',
+    preco: 20
+};
+//# sourceMappingURL=script.js.map
---
+++ src/script.ts
@@ -0,0 +1,11 @@
+import pluginSlide from "./pluginSlide.js";
+import { URL_BASE, Produto } from "./global.js";
+
+pluginSlide("div");
+
+URL_BASE;
+
+const livro: Produto = {
+  nome: 'salve',
+  preco: 20
+}
